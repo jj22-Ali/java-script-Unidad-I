2a281895631e1d34fbd1f79b25d95b4830d3aa27
Juan Jose <juanjosealifigueroa56@gmail.com>
2024-04-16 01:49:00 -0300

    arreglando el punto 20

--- js/punto20.js
+++ js/punto20.js
@@ -1,11 +1,22 @@
+//Realiza un script que pida cadenas de texto hasta que se pulse “cancelar”. Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión//
 
 var cadenaTexto = "";
 
 while(true){
-    var cadena = prompt("Escribra una palabra o frase: ;");
+    var texto = prompt("ingresar texto: ")
 
-    cadenaTexto += cadena + "-"
 
-};
+    if( texto === null){
+        break;
+    }
 
-document.write("La cadena quedaria asi: " + cadenaTexto);
+    cadenaTexto += texto + " - ";
+    
+
+}
+
+if(cadenaTexto.length > 0){
+    document.write("Texto conectado: ", cadenaTexto)
+}else{
+    document.write("Usted no ingreso nada.")
+}
